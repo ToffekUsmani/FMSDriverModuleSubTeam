c802986cbfa63d5ad1fc8c19ec14850d0d294247
Toffek Usmani <toffekusmani786@gmail.com>
2026-05-19 12:22:31 +0530

    Initial Commit

---
+++ FMSDriverModuleSubTeam/FMSDriverModuleSubTeamApp.swift
@@ -0,0 +1,17 @@
+//
+//  FMSDriverModuleSubTeamApp.swift
+//  FMSDriverModuleSubTeam
+//
+//  Created by Toffek Usmani on 19/05/26.
+//
+
+import SwiftUI
+
+@main
+struct FMSDriverModuleSubTeamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
